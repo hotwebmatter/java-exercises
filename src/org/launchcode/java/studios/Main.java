8631861529e4e0ce44c261ecd04215ed709d4164
Matt Obert <matt.obert@gmail.com>
2017-02-28 09:24:19 -0500

    Rename Area class Main.java in attempt to resolve ClassNotFound exception

---
+++ src/org/launchcode/java/studios/Main.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.studios;
+
+/**
+ * Created by modus on 2/27/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Area!");
+    }
+}
