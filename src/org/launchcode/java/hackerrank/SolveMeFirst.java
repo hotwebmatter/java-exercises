46757b19c09da3b4d857c842997d8209abeca22b
Matt Obert <matt.obert@gmail.com>
2017-03-01 20:39:15 -0500

    HackerRank Algorithm Challenges: Warmup 0

---
+++ src/org/launchcode/java/hackerrank/SolveMeFirst.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.hackerrank;
+
+/**
+ * Created by modus on 3/1/17.
+ * HackerRank Algorithm Challenges
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class SolveMeFirst {
+
+    static int solveMeFirst(int a, int b) {
+        // Hint: Type return a+b; below
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
